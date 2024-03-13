16b1c58dab938321599bd11fef025fc194b9ea53
NoodleBug <63314982+Noodle-Bug@users.noreply.github.com>
2024-03-13 17:06:05 -0500

    Adding sql browser extension

--- init.lua
+++ init.lua
@@ -112,6 +112,22 @@ local corePlugins = {
 	-- 'jiangmiao/auto-pairs', -- Auto match brackets
 	'mg979/vim-visual-multi', -- Multiple cursors
 	{
+		"kndndrj/nvim-dbee",
+		requires = {
+			"MunifTanjim/nui.nvim",
+		},
+		-- run = function()
+		-- 	-- Install tries to automatically detect the install method.
+		-- 	-- if it fails, try calling it with one of these parameters:
+		-- 	--    "curl", "wget", "bitsadmin", "go"
+		-- 	require("dbee").install()
+		-- end,
+		config = function()
+			-- require("dbee").install()
+			require("dbee").setup(--[[optional config]])
+		end
+	},
+	{
 		'2kabhishek/nerdy.nvim',
 		dependencies = {
 			'stevearc/dressing.nvim',
